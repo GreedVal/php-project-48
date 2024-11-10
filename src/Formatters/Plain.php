e8add45b457f9c192f85fd6e4ebd8dda225f900d
GreedVal <137034289+GreedVal@users.noreply.github.com>
2024-11-10 23:58:28 +0300

    greedval

--- src/Formatters/Plain.php
+++ src/Formatters/Plain.php
@@ -14,36 +14,37 @@ function formatDiff(array $diff, string $parentKey = ''): array
 {
     $result = [];
 
-    array_map(function ($node) use (&$result, $parentKey) {
-        $parentKeyTwo = $parentKey ? '.' : '';
+    if (empty($diff)) {
+        return $result;
+    }
 
-        $key = "{$parentKey}{$parentKeyTwo}{$node['key']}";
+    $node = array_shift($diff);
+    $parentKeyTwo = $parentKey ? '.' : '';
+    $key = "{$parentKey}{$parentKeyTwo}{$node['key']}";
 
-        $strValue1 = stringifyValue($node['value1']);
-        $strValue2 = stringifyValue($node['value2']);
+    $strValue1 = stringifyValue($node['value1'] ?? null);
+    $strValue2 = stringifyValue($node['value2'] ?? null);
 
-        switch ($node['status']) {
-            case 'nested':
-                $nestedResult = formatDiff($node['value1'], $key);
-                $result = array_merge($result, $nestedResult);
-                break;
-            case 'added':
-                $result[] = "Property '{$key}' was added with value: {$strValue1}";
-                break;
-            case 'removed':
-                $result[] = "Property '{$key}' was removed";
-                break;
-            case 'updated':
-                $result[] = "Property '{$key}' was updated. From {$strValue1} to {$strValue2}";
-                break;
-            case 'same':
-                break;
-            default:
-                throw new Exception("NAN no same");
-        }
-    }, $diff);
-
-    return $result;
+    switch ($node['status']) {
+        case 'nested':
+            $nestedResult = formatDiff($node['value1'], $key);
+            $result = array_merge($result, $nestedResult);
+            break;
+        case 'added':
+            $result[] = "Property '{$key}' was added with value: {$strValue1}";
+            break;
+        case 'removed':
+            $result[] = "Property '{$key}' was removed";
+            break;
+        case 'updated':
+            $result[] = "Property '{$key}' was updated. From {$strValue1} to {$strValue2}";
+            break;
+        case 'same':
+            break;
+        default:
+            throw new Exception("NAN error plain");
+    }
+    return array_merge($result, formatDiff($diff, $parentKey));
 }
 
 function stringifyValue(mixed $value): string

c1b7796690e0e22c8b229d9bb58e996826d7ee89
GreedVal <137034289+GreedVal@users.noreply.github.com>
2024-11-21 22:47:46 +0300

    greedval

--- src/Formatters.php
+++ src/Formatters.php
@@ -4,9 +4,9 @@ namespace Differ\Formatters;
 
 use Exception;
 
-use function Differ\Formatters\Json\format as stylishFormat;
+use function Differ\Formatters\Json\format as jsonFormat;
 use function Differ\Formatters\Plain\format as plainFormat;
-use function Differ\Formatters\Stylish\format as jsonFormat;
+use function Differ\Formatters\Stylish\format as stylishFormat;
 
 function makeFormat(array $diff, string $format): string
 {
